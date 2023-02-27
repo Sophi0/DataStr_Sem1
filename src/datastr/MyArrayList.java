71a1f561fd9fa320fdb4a5f3214025520e5c3c34
Sofja Hohlova <s21hohlsofj@vea.lv>
2023-02-27 11:22:50 +0200

    Edit: adding comment

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -78,11 +78,106 @@ public class MyArrayList {
 		return elementCounter;
 	}
 	
+	/*
+	 * Izveidot funkciju, kura izveido jaunu masīvu ar izmēru 2 reizes lielāku kā esošā 
+	 * masīva izmēru, pie nosacījuma - ja elementu skaits nav lielāks par 100, 
+	 * pretēja gadījumā izmēru palielina 1.5 reizes. Neaizmirstam pārkopēt visus 
+	 * elementus jaunizveidotajā masīvā.
+	 */
 	//TODO
 	//1.create a definition of function increaseArray
+		private void increaseArray() {
+			
 	//2.calculate the new size of the new array
+			int newArraySize = (arraySize > 100) ? (int)(arraySize * 1.5) : arraySize * 2;
+			
+			/*int newArraySize = arraySize * 2;
+			if(arraySize > 100) {
+				newArraySize = (int)(arraySize * 1.5);
+			}
+			*/
 	//3.create new array
+			char[] newElements = new char[newArraySize];
 	//4.copy all elements from old array to the new array
-	//5.change reference to the new array
+			for(int i = 0; i < elementCounter; i++) {
+				newElements[i] = elements[i];
+			}
+	//5.change reference to the new array and it's size
+			elements = newElements;
+			arraySize = newArraySize;
+		}
+		
+		
+	/*
+	 * Izveidot funkciju, kas veic elementu pievienošanu pēdējam saraksta 
+	 * elementam (veikt nepieciešamās pārbaudes).
+	 */
+		
+	//1.create a definition of function add
+		public void add(char newElement) {
+	//2.verify if the array is full
+			if(isFull()) {	//nevajag neko iekavas rakstit, jo pata funkcijair boolean tipa
+	//2.1.call an increaseArray() function
+				increaseArray();
+			}
+			
+	//3.add the new element in the array	
+	//the optimize option
+	elements[elementCounter++] = newElement;
+		
+	/*the non-optimize option
+	 *elements[elementCounter] = newElement;
+	 *elementCounter++;
+	*/
+			
+	//4.increase elementCounter
+		}
+		
+	//TODO
+	//1.create a definition of function add
+		public void add(char newElement, int index) throws Exception {
+	//2.verify the index - is it appropriate
+			if(index >= 0 && index <= elementCounter) {
+				
+	//3.verify if the index is equal with elementCounter
+				if(index == elementCounter) {
+					add(newElement);
+				}
+				else {
+	//3.1.verify isFull					
+					if(isFull()) {
+						increaseArray();
+					}
+				}
+		
+	//4.copy from the end to the right side(using index) ->for cikls
+				for(int i = elementCounter; i > index; i--) {
+					elements[i] = elements[i - 1];
+				}
+	//5.add the new element in the specified index
+				elements[index] = newElement;
+	//6.increase elementCounter			
+				elementCounter++;
+			}
+		//iznemums
+			else {
+				throw (new Exception("Wrong index"));
+			}
+		
+		}
+		
+	/*
+	 * Izveidot funkciju, kas veic elementu pievienošanu pēc indeksa 
+	 * (veikt nepieciešamās pārbaudes).
+	 */
+		
 	
+	//TODO
+	//1.create a definition of function add
+	//2.verify isEmpty
+	//3.verify the index - is it appropriate
+	//4.copy elements from index to end
+	//5.initialize the last element with NULL symbol (int value is 0)
+	//6.decrease element
+
 }
